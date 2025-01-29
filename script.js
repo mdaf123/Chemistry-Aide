4986c62c6c164407d221eebccb17acc80381da27
mdaf123 <140780519+mdaf123@users.noreply.github.com>
2025-01-28 19:31:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ var text;
 const button = document.getElementById('send');
 button.addEventListener('click', function() {
   input = document.getElementById('userInput').value;
-  text = '${input}';
+  text = String(input);
   document.getElementById('shiggidi').textContent = text;
 });
