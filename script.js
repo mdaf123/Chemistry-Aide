5db311fee21b25c289f53722c3b53b68dfefc987
mdaf123 <140780519+mdaf123@users.noreply.github.com>
2025-01-30 04:51:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ var text;
 const button = document.getElementById('send');
 button.addEventListener('click', function() {
   text = document.getElementById('shiggidi').textContent;
-  input = String(document.getElementById('userInput').value);
+  input = String(document.getElementById('userInput').value).trim();
   if (input == 'none') {
     text = 'Good Bye!'
   }
   document.getElementById('shiggidi').textContent = text;
+  document.getElementByID('userInput').value = '';
 });
