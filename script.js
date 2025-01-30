66a30120a44ae51e222e56293b91d83de8c02281
mdaf123 <140780519+mdaf123@users.noreply.github.com>
2025-01-30 05:35:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,29 @@ button.addEventListener('click', function() {
   input = String(document.getElementById('userInput').value).trim();
   if (input == 'none') {
     text = 'Good Bye!'
+  } else if(input == "12") {
+    percent_error();
+  } else () {
+    text = 'Please enter a valid input.'
   }
   document.getElementById('shiggidi').textContent = text;
   document.getElementByID('userInput').value = '';
 });
+
+function percent_error() {
+  while (true) {
+    try () {
+      document.getElementById('shiggidi').textContent = "What is the measured value?<br>";
+      var mv = String(document.getElementById('userInput').value).trim();
+      document.getElementByID('userInput').value = '';
+      document.getElementById('shiggidi').textContent += "What is the accepted value?<br>";
+      var av = String(document.getElementById('userInput').value).trim();
+      document.getElementByID('userInput').value = '';
+      var pv = Math.abs(parseFloat(mv) - parseFloat(av)) / parseFloat(av) * 100;
+      document.getElementById('shiggidi').textContent += "The percent error is " + String(pv) + "%<br>";
+      break
+    } catch(error) {
+      document.getElementById('shiggidi').textContent = "Please enter a valid number.";
+    }
+  }
+}
